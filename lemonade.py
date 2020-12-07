4bcc5f8582fea24876ebf59350c7effae141fbc7
stanleychow <chowtech1022@gmail.com>
2020-12-07 17:18:13 -0500

    Added Classes
    Classes: Lemonade, Register, Customer, Ingredients

--- lemonade.py
+++ lemonade.py
@@ -1,4 +1,15 @@
-def class Lemonade(self, sugar, size, price):
+class Lemonade(self, size, price):
     self.size = size
     self.price = price
+
+class Register(self, amount=100):
+    self.amount = amount
+
+class Customer(self, order)_size:
+    self.order_size = order_size
+
+class Ingredients(self, lemons=100, cups=100, sugar=1000):
+    self.lemons = lemons
+    self.cups = cups
     self.sugar = sugar
+    
